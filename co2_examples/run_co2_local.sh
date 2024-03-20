3716bbf9f4a5fece83fb9f1426e1fbec6c1671d8
Weigao Sun <sunweigao@outlook.com>
2024-03-20 05:42:22 +0000

    add run script

---
+++ co2_examples/run_co2_local.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/bash
+
+# export PATH=/usr/local/cuda-11.8/bin:$PATH
+# export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH
+
+BATCH_SIZE=2
+TOKENS_PER_SAMPLE=2048
+MAX_TOKEN=$((TOKENS_PER_SAMPLE*BATCH_SIZE))
+DATA_DIR=/mnt/data/NLP_3090/sunweigao/wiki103-jsonl
+ARCH=transformer_lm_gpt2_medium
+
+REMARK=CO2
+rm -rf checkpoints
+
+prefix=lm
+MAX_UPDATE=100000
+WARM_UP=2000
+# UPDATE_FREQ=$(( 128 / $BATCH_SIZE / 8 ))
+UPDATE_FREQ=1
+PORT=$(( $RANDOM + 2000 ))
+echo $PORT
+LR=0.0005
+CLIP_NORM=1.0
+decay=0.2
+
+logger_dir=./logs
+mkdir -p $logger_dir
+START_TIME=`date +%Y%m%d-%H:%M:%S`
+LOG_FILE=${logger_dir}/${ARCH}_${START_TIME}_$REMARK.log
+TENSOR_DIR=tensorboard/tensorboard-co2/$START_TIME-$ARCH-$REMARK
+
+torchrun --standalone --nproc_per_node=2 \
+    $(which fairseq-train) --task language_modeling \
+        $DATA_DIR --bpe gpt2 \
+        --save-dir checkpoints/$prefix/${ARCH} --tensorboard-logdir $TENSOR_DIR \
+        --arch $ARCH --clip-norm=$CLIP_NORM \
+        --ddp-backend pytorch_ddp \
+        --fp16 --bf16 --fp16-init-scale 4 --fp16-scale-window 128 --min-loss-scale 0.0001220703125 \
+        --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay $decay \
+        --lr $LR --lr-scheduler inverse_sqrt --warmup-updates $WARM_UP --warmup-init-lr 1e-08 \
+        --tokens-per-sample $TOKENS_PER_SAMPLE --sample-break-mode none \
+        --update-freq $UPDATE_FREQ \
+        --batch-size $BATCH_SIZE --num-workers 4 \
+        --max-update $MAX_UPDATE --log-format json --log-interval 1 2>&1 | sudo tee -a $LOG_FILE
